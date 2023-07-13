b9580e5fb37543a9c6fda95b601f35698e18673e
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2023-07-13 12:25:00 +0530

    commit

--- Blogging/Blogging/urls.py
+++ Blogging/Blogging/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Registraion',views.reg),
 ]
